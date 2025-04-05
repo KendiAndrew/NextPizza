a4578b158d1a99a87d1f2d83078faa634ce87ea3
Andrew <kenduhandrusa@gmail.com>
2025-04-05 16:49:36 +0300

    bugfix commit v1.05

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/NextPizza",
+  basePath: "/next-pizza",
   reactStrictMode: false,
   missingSuspenseWithCSRBailout: false,
 };
